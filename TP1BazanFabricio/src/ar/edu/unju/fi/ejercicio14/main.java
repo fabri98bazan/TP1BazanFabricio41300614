5942dbc58be52114e025486182e4b6b1238a79fd
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-04-09 11:24:15 -0300

    Subida del ejercicio 14

---
+++ TP1BazanFabricio/src/ar/edu/unju/fi/ejercicio14/main.java
@@ -0,0 +1,34 @@
+package ar.edu.unju.fi.ejercicio14;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		int n,i=0,aux,suma=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Ingrese un numero entero entre 3 y 10...");
+		do
+			n=sc.nextInt();
+		while(n<3 || n>10);
+		
+		int[] arreglo=new int[n];
+		System.out.println("Ingrese "+n+" numeros...");
+		
+		do
+		{
+			aux=sc.nextInt();
+			arreglo[i]=aux;
+			suma+=aux;
+			i++;
+		}
+		while(i<n);
+		
+		for(int j=0; j<n;j++)
+			System.out.println("El numero de la posicion "+j+" es: "+arreglo[j]);
+		
+		System.out.println("La suma total de los "+n+" numeros es: "+suma);
+		
+	}
+
+}
